179c3ac260137539d3a6c7c50560257eb531fb41
ChaosTempest812 <85038572+ChaosTempest812@users.noreply.github.com>
2021-08-07 20:01:19 -0400

    Create REQUIREMENTS.py

---
+++ REQUIREMENTS.py
@@ -0,0 +1,11 @@
+# Must have pip downloaded
+commands = ["pip install requests"]
+commands = ["pip install berserk"]
+commands = ["pip install numpy"]
+commands = ["pip install matplotlib"]
+
+# If pip is downloaded, check the current version
+commands = ["pip --version"]
+
+# If your pip is below the requirement (see prerequisites.py), update pip
+commands = ["pip install --upgrade pip"]
